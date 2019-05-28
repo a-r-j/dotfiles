6d692d405486b67cac250c2d25cb8766f4eb4479
Arian <arjamasb@gmail.com>
2019-05-29 00:23:49 +0100

    initial commit

---
+++ macos_install.sh
@@ -0,0 +1,21 @@
+cd ~/resources
+git submodule init
+git submodule update
+
+brew --version || /usr/bin/ruby -e "$curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master.install)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew tap homebrew/bundle
+brew update-reset && brew update
+brew bundle
+
+cd
+
+pip install --upgrade pip setuptools wheel virtualenv
+
+pip install --upgrade autopep8
+pip install --upgrade pex
+pip install --upgrade pygments
+pip install --upgrade virtualenv
+pip install --upgrade jedi
+
+
