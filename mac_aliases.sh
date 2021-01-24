3ca4cbafacd672d795a5eb948c71c2bccc52e3fd
a-r-j <arjamasb@gmail.com>
2021-01-24 22:42:49 +0000

    add sublime text

---
+++ mac_aliases.sh
@@ -0,0 +1,41 @@
+#Navigation
+alias ..='cd ..'
+alias cdg='cd ~/Documents/github/'
+alias cddw='cd ~/Downloads/'
+alias cddc='cd ~/Documents/'
+alias resources='cd ~/Documents/github/resources/'
+alias jio='cd ~/Documents/github/jamasb.io/'
+alias notes='cd ~/Documents/github/notes/'
+alias reading='subl ~/Documents/github/notes/reading.md'
+alias rowing='cd ~/Documents/github/rowing'
+
+#Git
+alias g='git'
+alias gall='git add *'
+alias gm='git commit -m' 
+alias pull='git pull'
+alias push='git push'
+alias pullarj='git pull notes; git pull resources; git pull jamasb.io'
+
+#Typos 
+alias lcoate='locate'
+alias grpe='grep --color=auto'
+alias grawl='graql'
+
+#Launching
+alias subl="open -a Sublime\ Text"
+alias todo="open -a ClickUp"
+alias slack="open -a Slack"
+alias rs="open -a RStudio"
+alias tg="open -a Telegram\ Desktop"
+alias tm="open -a TextMate"
+alias ical="open -a Calendar"
+alias em="open -a Mail"
+alias vs="open -a Visual\ Studio\ Code"
+
+#Options
+alias ll='ls -alhF --color=auto -F'
+alias la='ls -A'
+alias l='ls -CF --color=auto'
+alias cls='clear; ls' 
+alias grep='grep --color=auto'
