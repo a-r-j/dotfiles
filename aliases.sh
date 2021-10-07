32fb9e3f9ec9709f71714d24f6ea345796b034e6
Arian Jamasb <arjamasb@gmail.com>
2021-10-07 02:29:30 +0100

    refactor aliases

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,7 @@
 # Navigation
 alias ..='cd ..'
-alias cdg='cd ~/Documents/github/'
-alias cddw='cd ~/Downloads/'
+cddw='cd ~/Downloads/'
 alias cddc='cd ~/Documents/'
-alias resources='cd ~/Documents/github/resources/'
-alias jio='cd ~/Documents/github/jamasb.io/'
-alias notes='cd ~/Documents/github/notes/'
-alias reading='subl ~/Documents/github/notes/reading.md'
-alias rowing='cd ~/Documents/github/rowing'
 
 # Git
 alias g='git'
@@ -22,26 +16,13 @@ alias lcoate='locate'
 alias grpe='grep --color=auto'
 alias grawl='graql'
 
-# Launching
-# Mac:
-alias todo="open -a ClickUp"
-alias slack="open -a Slack"
-alias rs="open -a RStudio"
-alias tg="open -a Telegram\ Desktop"
-alias tm="open -a TextMate"
-alias ical="open -a Calendar"
-alias em="open -a Mail"
-alias vs="open -a Visual\ Studio\ Code"
-
-# Linux
-# alias zotero="cd " 
-
 # Options
 alias ll='ls -alhF --color=auto -F'
 alias la='ls -A'
 alias l='ls -CF --color=auto'
 alias cls='clear; ls' 
 alias grep='grep --color=auto'
+alias wget="wget -c"
 
 # Misc
 alias install_requirements='while read requirement; do conda install --yes $requirement; done < requirements.txt'
